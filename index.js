dc56c8650888cfb5c46735e1d8644e9347943e0e
Bushjdo <bushjdo@Bushjdos-MacBook-Pro.local>
2019-01-02 16:41:13 +0700

    added worker

--- index.js
+++ index.js
@@ -1,73 +1,26 @@
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const helpers = require('./lib/helpers');
-const { httpPort, httpsPort , envName} = require('./lib/config');
-const handlers = require('./lib/handlers/handlers');
+// Dependencies
 
-const httpServer = http.createServer(function(req, res){
-    unifiedServer(req,res)
-    
-})
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-httpServer.listen(httpPort, ()=> console.log(`Server is starting at ${httpPort} in ${envName} mode`));
+// Declare the app
 
-const httpsServerOption = {
-    cert: fs.readFileSync('./https/cert.pem'),
-    key: fs.readFileSync('./https/key.pem')
-}
-const httpsServer = https.createServer(httpsServerOption, function(req, res) {
-    unifiedServer(req,res)
-})
+const app = {};
 
+// Init function 
 
-httpsServer.listen(httpsPort, ()=> console.log(`Server is starting at ${httpsPort} in ${envName} mode`));
+app.init = function(){
+    // Start Server
+    server.init();
+    // Start Worker
 
-function unifiedServer(req, res) {
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/\/+|\/$/g, '');
-    const method = req.method.toLowerCase();
-
-    const queryStringObject = parsedUrl.query;
-    const headers = req.headers;
-
-    const decoder = new StringDecoder('utf-8');
-    let bufferString = ''; 
-
-    req.on('data', function(chunk){
-        bufferString += decoder.write(chunk);
-    })
-    req.on('end',function(){
-        bufferString += decoder.end();
-        
-        const choosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        const data = {
-            method,
-            queryStringObject,
-            trimmedPath,
-            headers,
-            payload: helpers.parseToJSONObject(bufferString)
-        }
-        choosenHandler(data, function(statusCode, payload) {
-            statusCode = typeof statusCode === 'number' ? statusCode : 200;
-            payload = typeof payload === 'object' ? payload : {};
-
-            res.writeHead(statusCode, {
-                'Content-Type': 'application/json'
-            });
-            payload = JSON.stringify(payload)
-            res.end(payload);
-        })
-        
-    });
-}
-const router = {
-    ping: handlers.ping,
-    users: handlers.users,
-    tokens: handlers.tokens,
-    checks: handlers.checks
+    workers.init();
 }
 
+// Execute app
+
+app.init();
+
+// Export app ( for test )
+
+module.exports = app;
